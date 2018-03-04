e650ea8d87cb22fb7e33f86c79c7fa95591dac89
axetroy <troy450409405@gmail.com>
2018-03-05 03:04:52 +0800

    test: add for in test case

--- test/ForInStatement.test.ts
+++ test/ForInStatement.test.ts
@@ -25,3 +25,28 @@ module.exports = obj;
   t.true(obj[1]);
   t.true(obj[2]);
 });
+
+test("ForInStatement-2", t => {
+  const sandbox: any = vm.createContext({});
+
+  const obj: any = vm.runInContext(
+    `
+const obj = {
+  1: false,
+  2: false
+};
+
+for (let attr in obj) {
+  if (obj.hasOwnProperty(attr)){
+    obj[attr] = true;
+  }
+}
+
+module.exports = obj;
+  `,
+    sandbox
+  );
+
+  t.true(obj[1]);
+  t.true(obj[2]);
+});
