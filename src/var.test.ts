b662877a03a5db606f2cd71e40512dce52a944eb
axetroy <troy450409405@gmail.com>
2018-03-29 03:26:58 +0800

    test: add test for var

---
+++ src/var.test.ts
@@ -0,0 +1,14 @@
+import test from "ava";
+import { Var } from "./var";
+
+test("var", t => {
+  const $var = new Var("var", "name", "hello world", {} as any);
+  t.deepEqual($var.kind, "var");
+  t.deepEqual($var.name, "name");
+  t.deepEqual($var.value, "hello world");
+  t.deepEqual($var.scope, {});
+
+  // set var
+  $var.set("hello");
+  t.deepEqual($var.value, "hello"); // value have been modify
+});
