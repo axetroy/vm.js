a5f8bf14c5a4aab4f11ed1befa93b94c7aa78b81
axetroy <troy450409405@gmail.com>
2018-04-30 19:20:24 +0800

    chore: pack it with webpack

---
+++ webpack.config.ts
@@ -0,0 +1,33 @@
+import * as webpack from "webpack";
+import * as path from "path";
+
+// webpack.config.js
+const webpackConfig: webpack.Configuration = {
+  entry: {
+    vm: path.join(__dirname, "src", "vm.ts")
+  },
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "[name].js"
+  },
+  resolve: {
+    modules: ["node_modules"],
+    extensions: [".ts", ".js"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader",
+        options: {
+          configFile: "tsconfig.webpack.json"
+        }
+      }
+    ]
+  },
+  devtool: "source-map",
+  mode: "production",
+  plugins: []
+};
+
+module.exports = webpackConfig;
